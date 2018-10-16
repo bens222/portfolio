e4db7ca2540bcd4f766e747f142c99ba0b1a1d73
Ben <bensims89@live.com>
2018-10-16 10:16:08 -0700

    first draft completed

--- server/server.js
+++ server/server.js
@@ -2,11 +2,19 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
+// Twilio requirements
+require('dotenv').load();
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
+
 const app = express();
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 app.set('views', './views');
 
@@ -15,6 +23,26 @@ app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
     res.render('index');
 });
+
+app.get('/projects', (req, res) => {
+    res.render('projects');
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
+app.post('/thanks', (req, res) => {
+    client.messages
+        .create({
+            body: `${req.body.firstName} ${req.body.lastName} is requesting to contact you with this email: ${req.body.email} and they included this message: ${req.body.message}`,
+            from: '+17606597150',
+            to: process.env.MY_PHONE_NUMBER
+        })
+    .then(message => console.log(message.sid))
+    .done();
+    res.render('thanks', { contact: req.body })
+})
 
 app.listen(8080, () => {
     console.log('listening at http://localhost:8080')
